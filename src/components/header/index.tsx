82e5283617f3394f04d86b3a5e8cefc54eae3efd
jonas souza <jonassouza305@yahoo.com.br>
2024-11-28 14:14:49 -0300

    adicionei link para vendas

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -2,6 +2,7 @@ import React, { useContext } from "react";
 import { NavBar } from "../navbar";
 import "./styled.css";
 import { ThemeContext, themes } from "@/theme-context/index";
+import ebookSales from "@/assets/vendasEbook.png"
 export const Headers = () => {
   const { theme, setTheme }: any = useContext(ThemeContext);
 
@@ -17,6 +18,12 @@ export const Headers = () => {
         >
           <img src={`${theme.img}`} />
         </button>
+        <div className="divVendas">
+          <a href="https://mercadolivre.com/sec/2CCwo1W" target="_blank" rel="noopener noreferrer">
+          <img src={`${ebookSales}`} alt="notebook" />
+          </a>
+          <p>Clique e garanta já o Guia definitivo sobre JavaScript avançado!</p>
+        </div>
       </header>
     </>
   );
