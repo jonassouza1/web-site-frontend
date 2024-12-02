e5525b77000e7792ca61684f1f5671ca4a515372
jonas souza <jonassouza305@yahoo.com.br>
2024-12-02 11:09:00 -0300

    foi implementado um carousel para sessao footer

--- src/components/footer/index.tsx
+++ src/components/footer/index.tsx
@@ -1,14 +1,44 @@
-import React from "react";
+import React, {useEffect,useRef} from "react";
 import imageBackground from "@/assets/parallax1.jpg";
 import digitalimg from "@/assets/digital.jpg";
 import "./styled.css";
 export const Footer = () => {
+
+  const leftBoxRef:any = useRef(null);
+  const rightBoxRef:any = useRef(null);
+
+  useEffect(() => {
+    // Seleciona as divs usando as referências
+    const boxes = [leftBoxRef.current, rightBoxRef.current];
+
+    // Cria o Intersection Observer
+    const observer = new IntersectionObserver((entries, observer) => {
+      entries.forEach(entry => {
+        if (entry.isIntersecting) {
+          // Quando a div entra na tela, adiciona a classe 'visible'
+          entry.target.classList.add('visible');
+          observer.unobserve(entry.target); // Para de observar depois que a animação acontecer
+        }
+      });
+    }, {
+      threshold: 0.5, // A div precisa estar 50% visível para iniciar a animação
+    });
+
+    // Observa as divs
+    boxes.forEach(box => observer.observe(box));
+
+    // Limpeza do observer ao desmontar o componente
+    return () => {
+      boxes.forEach(box => observer.unobserve(box));
+    };
+  }, []); 
+
   return (
     <footer style={{ backgroundImage: `url(${imageBackground})` }}>
-      <div className="imgContat">
+      <div ref={leftBoxRef} className="imgContat box">
         <img src={`${digitalimg}`} alt="image digital" />
       </div>
-      <div className="infoContat">
+      <div ref={rightBoxRef} className="infoContat box">
         <a
           href="https://www.linkedin.com/in/jonas-souza-dev-fullstack"
           target="_blank"
